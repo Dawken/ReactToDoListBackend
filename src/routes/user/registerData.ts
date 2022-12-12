786870f119618edf827967275cdf564ee0fe6a67
Dawid Domiński <dawid.dominski1125@gmail.com>
2022-12-12 02:01:03 +0100

    feat: check password strength and user login exists

--- src/routes/user/registerData.ts
+++ src/routes/user/registerData.ts
@@ -1,5 +1,7 @@
 import {RegisterDTO} from './registerDTO'
 import {bodyValidator} from '../../shared/bodyValidator'
+import * as mongoose from 'mongoose'
+import {response} from 'express'
 const bcrypt = require('bcrypt')
 
 const formModel = require('./registerModel')
@@ -13,6 +15,15 @@ formRouter.post('/api/register', async(req, res) => {
 		date18YearsAgo.setFullYear(date18YearsAgo.getFullYear()-18)
 		if(new Date(birth) >= date18YearsAgo) {
 			return res.status(400).json({message: 'You have to be at least 18 years old'})
+		}
+		const userLogin = await formModel.findOne({login: req.body.login}, 'login').exec()
+		if(userLogin) {
+			return res.status(400).json({message:'User already exist!'})
+		}
+		if(!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/.test(req.body.password)) {
+			return res.status(400).json({
+				message: '\'Password need to has at least 1 Capital letter, 1 special symbol, 1 number and be 8 symbols long\''
+			})
 		}
 		const hashedPassword = bcrypt.hashSync(req.body.password, 10)
 		const data = new formModel({
@@ -30,13 +41,13 @@ formRouter.post('/api/register', async(req, res) => {
 		res.status(400).json({message: error.message})
 	}
 })
-formRouter.get('/api/register', async(req, res) => {
+formRouter.post('/api/login', async(req, res) => {
 	try {
-		const data = await formModel.find()
-		res.json(data)
+		const password = await formModel.findOne({login: req.body.login}, 'password').exec()
+		res.status(200).json(bcrypt.compareSync(req.body.password, password.password))
 	}
-	catch(error) {
-		res.status(400).json({message: error.message})
+	catch (error) {
+		console.log(error)
 	}
 })
 
