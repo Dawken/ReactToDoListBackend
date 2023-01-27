ba92396dd71ce4eb0de7a25f4313563a650c79cf
Dawken <dawid.dominski1125@gmail.com>
2023-01-27 18:02:06 +0100

    fix: fix register gender and last name validation

--- src/routes/userAccount/registerDTO.ts
+++ src/routes/userAccount/registerDTO.ts
@@ -1,37 +1,36 @@
-import {IsEnum, IsString, Length, Matches} from 'class-validator'
+import { IsEnum, IsOptional, IsString, Length, Matches } from 'class-validator'
 
 enum Gender {
 	Male = 'Male',
-    Female = 'Female',
-    Other = 'Other'
+	Female = 'Female',
+	Other = 'Other',
 }
 interface Register {
-    login: string
-    name: string
-    lastName: string
-    password: string,
-    gender: string
+	login: string;
+	name: string;
+	lastName: string;
+	password: string;
+	gender: string;
 }
 
 export class RegisterDTO implements Register {
-
-    @Length(3, 16)
+	@Length(3, 16)
 	@IsString()
-    	login: string
-
-	@Length(3)
-	@IsString()
-    	name: string
+		login: string
 
 	@Length(3)
 	@IsString()
-    	lastName: string
+		name: string
+
+	@Length(3)
+	@IsString()
+		lastName: string
 
 	// Check password includes at least 1 capital letter, 1 special symbol and has at least 8 symbols
 	@Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/)
-    	password: string
+		password: string
 
-    @IsEnum(Gender)
-    	gender: Gender
+	@IsEnum(Gender)
+	@IsOptional()
+		gender: Gender
 }
-
