be0b06147fad28b632d9a8b5f81062583139c8d3
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-02-07 13:27:28 +0100

    feat: update .env.examples and mongoDbUrl with database connection string

--- src/shared/mongoDbUrl.ts
+++ src/shared/mongoDbUrl.ts
@@ -1,5 +1,14 @@
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
+const {
+	DB_USER,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_PORT,
+	DB_NAME,
+	MONGO_CONNECTED_STRING,
+} = process.env
 
 export const mongoDb = {
-	url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+	url:
+		MONGO_CONNECTED_STRING ??
+		`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
 }
